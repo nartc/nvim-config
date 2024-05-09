786d9811ed70ff5f93640d505bbc2998360da575
Chau Tran <nartc7789@gmail.com>
2024-05-08 22:02:54 -0500

    fix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,8 +3,8 @@
 -- Add any additional keymaps here
 
 -- move lines up and down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- remap save to save all
 vim.keymap.set("n", ":w", ":wa")
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,5 +1,8 @@
 return {
   "folke/noice.nvim",
+  -- TODO: remove when PR is merged: https://github.com/folke/noice.nvim/pull/785
+  url = "https://github.com/wookayin/noice.nvim.git",
+  branch = "lsp-progress",
   opts = function(_, opts)
     opts.presets.lsp_doc_border = true
   end,
